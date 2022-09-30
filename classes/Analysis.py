6934496a901d8388b84cc002b7eb19b62f6989b2
Lebedev Vladimir <goldswanru@gmail.com>
2022-09-30 04:13:19 +0300

    Initial commit

---
+++ classes/Analysis.py
@@ -0,0 +1,7 @@
+class Analysis:
+
+    def statistics(self, N, data): # Task 3
+        pass
+
+    def stationary(self, N, data): # Task 3
+        pass
---
+++ classes/Model.py
@@ -0,0 +1,20 @@
+class Model:
+
+    def trend(self, N, a, b, data): # Task 1
+
+        #for i in range(0, N):
+
+
+        pass
+
+    def noise(self, N, R, data): # Task 2
+        pass
+
+    def myNoise(self, N, R, data): # Task 2
+        pass
+
+    def shift(self, inData, N, C, outData): # Task 4
+        pass
+
+    def impulseNoise(self, data, N, M, R, Rs): # Task 4
+        pass
